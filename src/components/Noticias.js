ab42942cb972425430094bfec879d93d4bacfde1
Paula Herrera <paula.herrera@est.iudigital.edu.co>
2022-07-15 19:40:58 -0500

    actualiza noticia.js

---
+++ src/components/Noticias.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Noticia from './Noticia'
+ 
+export default function Noticias({noticias}) {
+  return (
+    <div className="row row-cols-1 row-cols-md-2 g-4 my-2">
+    {noticias.map((noticia, index) => {
+        return (<Noticia key={index} noticia={noticia}/>);
+    })}
+</div>
+  )
+}
+ 
---
+++ src/config.js
@@ -0,0 +1,16 @@
+/**
+ * Base URl
+ */
+export const API_URL = 'https://newsapi.org/v2';
+
+/**
+ * Countru
+ */
+
+export const API_COUNTRY = 'co';
+
+/**
+ * Autor
+ */
+
+export const AUTOR = 'Paula Herrera';
